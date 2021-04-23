29a0d3a05f3c733866229be5cfce8ace781e6dbd
Luis Gustavo Monteiro <lgmluisgm@gmail.com>
2021-04-23 07:22:23 -0300

    :tada: finish game

--- README.md
+++ README.md
@@ -2,5 +2,31 @@
 
 ![Design Final do Jogo](./imgs/design-flappy.jpeg)
 
-Projeto construído durante o curso de Web Moderno
+Projeto construído durante o curso sobre Web
+
+---
+
+## Sumary 📔
+
+- [Flappy Bird Project](#flappy-bird-project)
+  - [Sumary 📔](#sumary-)
+  - [About it](#about-it)
+  - [Technologies](#technologies)
+
+
+---
+
+## About it
+
+This project was basically developed during the study of the modern Web Course. It is basically a replica of Flappy Bird, an old mobile game 📱🐦. The idea of the project is basically just training dom events, about parentship of html elements and somethings about POO, it's not literally POO but it's almost. 
+
+---
+
+## Technologies
+
+- HTML
+- CSS3
+- Javascript
+
+
 
--- flappy.js
+++ flappy.js
@@ -45,7 +45,7 @@ function parDeBarreiras(altura, abertura, position)
     this.setPos(position)
 }
 
-function barreiras(altura, largura, abertura, espaco, notficarPonto)
+function Barreiras(altura, largura, abertura, espaco, notficarPonto)
 {
     this.pares = 
     [
@@ -55,7 +55,7 @@ function barreiras(altura, largura, abertura, espaco, notficarPonto)
         new parDeBarreiras(altura, abertura, largura + espaco * 3),
     ]
 
-    const deslocamento = 3
+    const deslocamento = 5
 
     this.animar = () =>
     {
@@ -116,8 +116,74 @@ function Bird(alturaDatela)
 
 }
 
+function Sobrepostos(elemA, elemB) 
+{
+    const a = elemA.getBoundingClientRect()
+    const b = elemB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >=b.left 
+                       && b.left + b.width >= a.left 
+    const vertical = a.top + a.height >=b.top 
+                       && b.top + b.height >= a.top 
+
+    return horizontal && vertical
+}
+
+function colision (bird, bars)
+{
+    let colisao = false
+
+    bars.pares.forEach( par => {
+        if(!colisao)
+        {
+            const sup = par.sup.element
+            const inf = par.sup.element
+            colisao = Sobrepostos(bird.element, sup) || Sobrepostos(bird.element, inf) 
+        }
+
+    })
+
+    return colisao
+}
 
 
+function Score() 
+{
+    this.element = newElement('span', 'score')
+    this.atualizarPontos = pontos => this.element.innerHTML = `${pontos}`
+
+    this.atualizarPontos(0)
+}
+
+function flappyBird () 
+{
+    let pontos = 0
+    const gameArea = document.querySelector('[wm-flappy]')
+    const altura = gameArea.clientHeight
+    const largura = gameArea.clientWidth
+
+    const score = new Score()
+    const barreiras = new Barreiras(altura, largura, 200, 400, () => score.atualizarPontos(++pontos))
+    const bird = new Bird(altura)
+
+    gameArea.appendChild(score.element)
+    gameArea.appendChild(bird.element)
+    barreiras.pares.forEach(par => gameArea.appendChild(par.element))
+
+    this.start = () => {
+        const temp = setInterval(() => {
+            barreiras.animar()
+            bird.animar()
+            
+            if(colision(bird, barreiras))
+            {
+                clearInterval(temp)
+            }
+        }, 20)
+    }
+}
+
+new flappyBird().start()
 
 
 
